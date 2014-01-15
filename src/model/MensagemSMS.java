2c50892c4679e828f0891d878b169dc5168a8824
MarcelorjOliveira <marcelorj.oliveira@gmail.com>
2014-01-15 10:10:43 -0200

    Final

--- src/model/MensagemSMS.java
+++ src/model/MensagemSMS.java
@@ -2,43 +2,54 @@ package model;
 
 public class MensagemSMS {
 
-	private String codigos;
+	private String codigos = "";
+	private int botao;
 	
 	public MensagemSMS(String mensagem) {
-		codigos = "";
 		for(int contador = 0 ; contador < mensagem.length(); contador++){
-			codigos = codigos + compila(mensagem.charAt(contador));
+			
+			int botaoAnterior = botao;
+			
+			String sequencia = compila(mensagem.charAt(contador));
+			int botaoAtual = botao;
+			
+			if((botaoAnterior == botaoAtual) && (botaoAnterior != 0)) {
+				codigos = codigos + "_";
+			}
+			
+			codigos = codigos + sequencia;
+			
 		}
 	}
 	
 	public String compila(char caracter){
 		switch(caracter){
-			case 'A': return "2";
-			case 'B': return "22";
-			case 'C': return "222";
-			case 'D': return "3";
-			case 'E': return "33";
-			case 'F': return "333";
-			case 'G': return "4";
-			case 'H': return "44";
-			case 'I': return "444";
-			case 'J': return "5";
-			case 'K': return "55";
-			case 'L': return "555";
-			case 'M': return "6";
-			case 'N': return "66";
-			case 'O': return "666";
-			case 'P': return "7";
-			case 'Q': return "77";
-			case 'R': return "777";
-			case 'S': return "7777";
-			case 'T': return "8";
-			case 'U': return "88";
-			case 'V': return "888";
-			case 'W': return "9";
-			case 'X': return "99";
-			case 'Y': return "999";
-			case 'Z': return "9999";
+			case 'A': botao = 2; return "2";
+			case 'B': botao = 2; return "22";
+			case 'C': botao = 2; return "222";
+			case 'D': botao = 3; return "3";
+			case 'E': botao = 3; return "33";
+			case 'F': botao = 3; return "333";
+			case 'G': botao = 4; return "4";
+			case 'H': botao = 4; return "44";
+			case 'I': botao = 4; return "444";
+			case 'J': botao = 5; return "5";
+			case 'K': botao = 5; return "55";
+			case 'L': botao = 5; return "555";
+			case 'M': botao = 6; return "6";
+			case 'N': botao = 6; return "66";
+			case 'O': botao = 6; return "666";
+			case 'P': botao = 7; return "7";
+			case 'Q': botao = 7; return "77";
+			case 'R': botao = 7; return "777";
+			case 'S': botao = 7; return "7777";
+			case 'T': botao = 8; return "8";
+			case 'U': botao = 8; return "88";
+			case 'V': botao = 8; return "888";
+			case 'W': botao = 9; return "9";
+			case 'X': botao = 9; return "99";
+			case 'Y': botao = 9; return "999";
+			case 'Z': botao = 9; return "9999";
 			case ' ': return "0";
 		}
 		/*return sequencia; */
--- src/model/SMSTest.java
+++ src/model/SMSTest.java
@@ -6,13 +6,12 @@ import org.junit.Test;
 
 public class SMSTest {
 	
-	/*
-	@Test
+	/*@Test
 	public void testaTexto(){
 		MensagemSMS mensagem = new MensagemSMS("QUERO TRABALHAR NA WDEV");
 		assertEquals("778833777666087772_22_25554427770662093_33888", mensagem.getCodigos());
-	}
-	*/
+	}*/
+	
 	@Test
 	public void testaLetraA(){
 		MensagemSMS letraA = new MensagemSMS("A");
@@ -29,6 +28,12 @@ public class SMSTest {
 	public void testaEspaco(){
 		MensagemSMS espaco = new MensagemSMS(" ");
 		assertEquals("0", espaco.getCodigos());
+	}
+	
+	@Test
+	public void testaAA(){
+		MensagemSMS aa = new MensagemSMS("AA");
+		assertEquals("2_2", aa.getCodigos());
 	}
 
 }
